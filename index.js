4373f93cc337ddf58b83c06d448eb05a4424ac04
Joseph Abel <josephabel540@gmail.com>
2025-09-13 18:43:40 +0100

    first commit

--- index.js
+++ index.js
@@ -1,20 +1,17 @@
-const themeToggle = 
+const themeToggle = document.getElementById('theme-toggle');
+const mobileThemeToggle = document.getElementById('mobile-theme-toggle');
+const body = document.body;
+const mobileMenuButton = document.getElementById('mobile-menu-button');
+const mobileMenu = document.getElementById('mobile-menu');
 
-document.getElementById('theme-toggle');
-        const mobileThemeToggle = document.getElementById('mobile-theme-toggle');
-        const body = document.body;
-
-        themeToggle.addEventListener('click', () => {
-            body.classList.toggle('dark');
-        });
-
-        mobileThemeToggle.addEventListener('click', () => {
-            body.classList.toggle('light');
-        });
-
-        const mobileMenuButton = document.getElementById('mobile-menu-button');
-        const mobileMenu = document.getElementById('mobile-menu');
-
-        mobileMenuButton.addEventListener('click', () => {
-            mobileMenu.classList.toggle('hidden');
-        });
+themeToggle.addEventListener('click', () => {
+    body.classList.toggle('dark');
+});
+mobileThemeToggle.addEventListener('click', () => {
+    body.classList.toggle('dark');
+}
+);
+mobileMenuButton.addEventListener('click', () => {
+    mobileMenu.classList.toggle('hidden');
+}
+);            
